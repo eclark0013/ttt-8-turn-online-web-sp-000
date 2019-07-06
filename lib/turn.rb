986cee7f7a298eb77ef227773ebb916c7ef68a7c
Eric Clark <eclark0013@gmail.com>
2019-07-06 15:26:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,7 @@
 def turn(board)
     puts "Please enter a number 1-9:"
     spot = input_to_index(gets)
-    character = current_player(board)
+    character = "X"
   until valid_move?(board,spot)
     puts "Please enter a number 1-9:"
     spot = input_to_index(gets)
@@ -34,7 +34,7 @@ def good_number(index)
   index.between?(0, 8)
 end
 
-def move(board,index,character = "X")
+def move(board,index,character)
   board[index]="#{character}"
   board
 end
