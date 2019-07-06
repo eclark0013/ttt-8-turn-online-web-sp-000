7448c8c10c23545ad29f655ecbfb3eae71e172d2
Eric Clark <eclark0013@gmail.com>
2019-07-06 15:23:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,27 @@ def turn(board)
   display_board(move(board, spot, character))
   move(board, spot, character)
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, position)
+  good_number(position) && !(position_taken?(board, position))
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def good_number(index)
+  index.between?(0, 8)
+end
